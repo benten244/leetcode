3b2361c4a4e56cc5b7e79d43dd172ab678ed6292
benten244 <tumemura@netprotecitons.co.jp>
2024-11-14 22:21:44 +0800

    Answered

--- problem lists/easy/roman_to_integer.rb
+++ problem lists/easy/roman_to_integer.rb
@@ -1,18 +1,59 @@
 # @param {String} s
 # @return {Integer}
 
-# I             1
-# V             5
-# X             10
-# L             50
-# C             100
-# D             500
-# M             1000
 
+def roman_to_int(roman)
 
-def roman_to_int(s)
+  allocation = {
+      'I' => 1,
+      'V' => 5,
+      'X' => 10,
+      'L' => 50,
+      'C' => 100,
+      'D' => 500,
+      'M' => 1000
+  }
 
-  if s.in?('I', 'X', 'C', 'M')
+  minus_allocation = {
+      'IV' => -2,
+      'IX' => -2,
+      'XL' => -20,
+      'XC' => -20,
+      'CD' => -200,
+      'CM' => -200
+  }
+
+  minus_array = ['IV', 'IX', 'XL', 'XC', 'CD', 'CM']
+
+  @total_num = 0
+  @minus_roman = 0
+  rejected_array = []
+  n = []
+
+  minus_array.each do |ma|
+      if roman.include?(ma)
+          rejected_array << ma
+      end
   end
 
+  minus_amount = []
+  if rejected_array
+      rejected_array.each do |a|
+          numnum = minus_allocation[a]
+          minus_amount << numnum
+      end
+  end
+
+ @minus_amount =  minus_amount.empty? ? 0 : minus_amount.sum
+
+  roman.length.times do |i|
+      n << roman[i]
+  end
+
+  n.each do |num|
+      new_num = allocation[num].to_i
+      @total_num += new_num
+  end
+
+  (@total_num + @minus_amount)
 end
