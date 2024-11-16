bb5aa8b795df76bbf77827d6b4659adfca331599
benten244 <tumemura@netprotecitons.co.jp>
2024-11-16 20:11:37 +0800

    answerd

---
+++ problem lists/easy/reverse_linked_list.rb
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} head
+# @return {ListNode}
+def reverse_list(head)
+
+  integer_arr = []
+
+  while head != nil
+      integer_arr << head.val.to_i
+
+      head = head.next
+  end
+
+  integer_arr.reverse
+
+end
