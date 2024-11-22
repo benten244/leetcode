e440fb917e46706304e9bf83da07864f688c744d
benten244 <tumemura@netprotecitons.co.jp>
2024-11-22 21:16:12 +0800

    Answerd

--- problem lists/easy/palindrome_number.rb
+++ problem lists/easy/palindrome_number.rb
@@ -1,25 +1,20 @@
-def is_palindrome(x = 13531)
+# @param {Integer} x
+# @return {Boolean}
+def is_palindrome(num)
 
-  #1. 絶対にないケースを弾く
-  return false if x < 0
-  return false if (x % 10).zero?
+  result = false
 
-  reversed_num = 0
+  return result if num < 0
 
-  while x > reversed_num
+  num_arr = []
 
-    # いちばん最後の桁を取得
-    digit = x % 10
+  @num = num.to_s.split("")
 
-    # 桁を上げて、最後日に数字を入れる
-    reversed_num = reversed_num * 10 + digit
+  @num_reverse = @num.reverse
 
-    # 桁を下げる
-    x /= 10
-  end
+  result = true if @num == @num_reverse
+  result = false if @num != @num_reverse
 
-  # 桁数が偶数の場合は、reversed_numとxが同じになる
-  # 桁数が奇数の場合は、reversed_numとxが同じになる
-  x == reversed_num || x == reversed_num / 10
-
+  result
 end
+
