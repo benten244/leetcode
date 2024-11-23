850ecc55ad61343e14fb584914d5d36c4a1249d1
benten244 <tumemura@netprotecitons.co.jp>
2024-11-23 19:58:48 +0800

    Answered

---
+++ problem lists/easy/remove_element.rb
@@ -0,0 +1,20 @@
+# @param {Integer[]} nums
+# @param {Integer} val
+# @return {Integer}
+def remove_element(nums, val)
+  p "nums: #{nums}"
+  p "val: #{val}"
+
+  index = 0
+
+  nums.each do |n|
+      next if n == val
+      nums[index] = n
+      index += 1
+  end
+
+
+  # nums.countにすると、削除された要素も含めたカウントを返す
+  # indexにすることで、現存要素だけを返している。
+  index
+end
