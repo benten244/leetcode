942efc46cc5b5dd3a4adc09dfaa81374f1ce9ac0
benten244 <tumemura@netprotecitons.co.jp>
2024-12-01 21:41:45 +0800

    Answered

---
+++ problem lists/easy/length_of_last_word.rb
@@ -0,0 +1,11 @@
+# @param {String} s
+# @return {Integer}
+def length_of_last_word(s)
+
+  # スペースごとに配列にする。
+  string_arr = s.split(" ")
+  result_string_arr = string_arr.reverse
+
+  result_string_arr[0].length
+
+end
