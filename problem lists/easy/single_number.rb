4306e214bcd614271bf788cbbac5e3b1ee70ac6b
benten244 <tumemura@netprotecitons.co.jp>
2024-12-07 18:37:36 +0800

    Answered

---
+++ problem lists/easy/single_number.rb
@@ -0,0 +1,24 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def single_number(nums)
+
+  p nums = nums.sort
+
+  index = 0
+
+  result = nil
+
+  p nums.count
+
+  nums.count.times do |n|
+
+      result = nums[index] if nums[index] != nums[index + 1]
+
+      break if !result.nil?
+
+      index += 2
+  end
+
+  result
+
+end
