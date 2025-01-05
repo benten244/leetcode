84122c5e75139f9ae754f1ca188a85aaed29cf0b
benten244 <tumemura@netprotecitons.co.jp>
2025-01-05 17:32:33 +0800

    Atcoder

---
+++ atcoder/2025/01/04/9_x_9_sum.rb
@@ -0,0 +1,17 @@
+n = gets.to_i
+
+vertical_arr = (1..9).to_a
+horizontal_arr = (1..9).to_a
+
+sum_arr = []
+
+vertical_arr.count.times do |i|
+  horizontal_arr.count.times do |j|
+    sum_arr << vertical_arr[i] * horizontal_arr[j]
+  end
+end
+
+sum_arr = sum_arr.reject { |s| s == n }
+
+result = sum_arr.sum
+puts result
---
+++ atcoder/2025/01/04/snake_numbers.rb
@@ -0,0 +1,12 @@
+a, b = gets.split.map(&:to_i)
+numbers = (a..b).to_a
+
+result_flag_count = 0
+
+numbers.each do |n|
+  n.split("").each do |s|
+    result_flag_count += 1 if n.select { |s| s > n[0] }.present?
+  end
+end
+
+puts result_flag_count
