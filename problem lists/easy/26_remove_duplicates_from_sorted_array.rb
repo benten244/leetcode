824213495b8c4dcf221ed4cd4fe39761c845c373
benten244 <tumemura@netprotecitons.co.jp>
2025-01-26 21:23:25 +0800

    answered

---
+++ problem lists/easy/26_remove_duplicates_from_sorted_array.rb
@@ -0,0 +1,16 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def remove_duplicates(nums)
+  return 0 if nums.empty?
+
+  i = 0
+
+  nums.each_with_index do |num, index|
+      if index == 0 || nums[index] != nums[index - 1]
+          nums[i] = num
+          i += 1
+      end
+  end
+
+  i
+end
