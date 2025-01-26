570af81e48c7a75da66f285a2c529dc7d3f52bb5
benten244 <tumemura@netprotecitons.co.jp>
2025-01-26 21:42:13 +0800

    answered

---
+++ problem lists/easy/88_merge_sorted_array.rb
@@ -0,0 +1,42 @@
+# @param {Integer[]} nums1
+# @param {Integer} m
+# @param {Integer[]} nums2
+# @param {Integer} n
+# @return {Void} Do not return anything, modify nums1 in-place instead.
+def merge(nums1, m, nums2, n)
+
+  return nums1 if n == 0
+
+  pointa = -1
+
+  arr_count = (nums1.count - m)
+
+  arr_count.times do
+      nums1[pointa] = nums2[pointa]
+      pointa -= 1
+  end
+
+  nums1.sort!
+end
+
+
+# def merge(nums1, m, nums2, n)
+
+#     return nums1 if n == 0
+
+#     arr_count = (nums1.count - m)
+
+#     starting_index = m
+
+#     index = 0
+
+#     arr_count.times do
+#         nums1[starting_index] = nums2[index]
+#         starting_index += 1
+#         index += 1
+#     end
+
+#     p nums1
+
+#     nums1.sort!
+# end
