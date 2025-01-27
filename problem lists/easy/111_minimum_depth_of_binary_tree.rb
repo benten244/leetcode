a99f9f77216dce62c89581265e9796e1d0006293
benten244 <tumemura@netprotecitons.co.jp>
2025-01-27 18:49:39 +0800

    answered

---
+++ problem lists/easy/111_minimum_depth_of_binary_tree.rb
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer}
+def min_depth(root)
+  return 0 if root.nil?
+  return 1 if root.right.nil? && root.left.nil?
+
+  if root.left.nil?
+      return min_depth(root.right) + 1
+  elsif root.right.nil?
+      return min_depth(root.left) + 1
+  end
+
+  [min_depth(root.left), min_depth(root.right)].min + 1
+end
+
+
+#  || (root.right.nil? && root.left.nil? )
