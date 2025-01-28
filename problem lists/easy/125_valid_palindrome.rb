3a40a108894d84f6fa90c6f6ce278bcbeb03bb0d
benten244 <tumemura@netprotecitons.co.jp>
2025-01-28 22:57:17 +0800

    answered

--- problem lists/easy/125_valid_palindrome.rb
+++ problem lists/easy/125_valid_palindrome.rb
@@ -7,3 +7,21 @@ def is_palindrome(s)
 
   result
 end
+
+# @param {String} s
+# @return {Boolean}
+def is_palindrome(s)
+  p string = s.downcase.gsub(/[^a-z0-9]/, "")
+
+  initial = 0
+  terminal = string.length - 1
+
+  while initial < terminal
+      return false if string[initial] != string[terminal]
+
+      initial += 1
+      terminal -= 1
+  end
+
+  true
+end
