f417b9dced883029cb381a9b433bc56bc808fad7
benten244 <tumemura@netprotecitons.co.jp>
2025-01-29 18:09:09 +0800

    mod

---
+++ problem lists/easy/112_path_sum.rb
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @param {Integer} target_sum
+# @return {Boolean}
+
+
+def has_path_sum(root, target_sum)
+  return false if root.nil?
+
+  target_sum -= root.val
+
+  return true if (target_sum == 0 && (root.right.nil? && root.left.nil?))
+
+  (has_path_sum(root.right, target_sum) || has_path_sum(root.left, target_sum))
+end
