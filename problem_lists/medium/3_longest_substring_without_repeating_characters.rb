47a9ca7884790588443356922da2da859944452b
benten244 <tumemura@netprotecitons.co.jp>
2025-04-21 19:26:15 +0800

    mod

--- problem_lists/medium/3_longest_substring_without_repeating_characters.rb
+++ problem_lists/medium/3_longest_substring_without_repeating_characters.rb
@@ -7,29 +7,27 @@ def length_of_longest_substring(s)
   pre_p = 0
   fin_p = s.empty? ? 0 : 1
 
-  chars_hash = Hash.new(0)
-  max_length = 0
+#   chars_hash = Hash.new(0)
+#   max_length = 0
+#   while fin_p < @string.length
+#     split_string = @string[pre_p .. fin_p]
 
+#     if is_duplicated?(split_string)
+#       pre_p += 1
+#       next
+#     end
 
-  while fin_p < @string.length
-    split_string = @string[pre_p .. fin_p]
+#     chars_hash[split_string] += 1
 
-    if is_duplicated?(split_string)
-      pre_p += 1
-      next
-    end
+#     if chars_hash[split_string] == 1
+#       max_length = [max_length, split_string.length].max
+#     end
 
-    chars_hash[split_string] += 1
+#     fin_p += 1
+#   end
 
-    if chars_hash[split_string] == 1
-      max_length = [max_length, split_string.length].max
-    end
-
-    fin_p += 1
-  end
-
-  max_length
-end
+#   max_length
+# end
 
 def is_duplicated?(string)
   checking_hash = Hash.new(0)
@@ -40,3 +38,25 @@ def is_duplicated?(string)
 
   false
 end
+
+def length_of_longest_substring(s)
+  return 0 if s.empty?
+
+  chars_hash = {}
+  max_length = 0
+  pre_p = 0
+
+  s.chars.each_with_index do |char, fin_p|
+    if chars_hash.key?(char) && chars_hash[char] >= pre_p
+      pre_p = chars_hash[char] + 1
+    end
+    max_length = [max_length, fin_p - pre_p + 1].max
+    chars_hash[char] = fin_p
+  end
+
+  max_length
+end
+
+
+arguement = "abcabcbb"
+length_of_longest_substring(arguement)
